fecc5f38d524d387e0a474af33bf826069425980
MinwookJo <alsdnr0537@naver.com>
2024-06-07 18:10:40 +0900

    init

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TestComponent from './lib/TestComponent'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <TestComponent/>
+  </React.StrictMode>,
+)
